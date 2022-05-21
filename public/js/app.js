5ef29d4eb6c1ecf24946af4d440b5c7bf3ee38af
alexey-ringo <alexey.ringo@gmail.com>
2022-05-21 23:05:05 +0000

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -56872,8 +56872,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/alex/projects/testjob/4pixels/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/alex/projects/testjob/4pixels/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/alex/projects/testjob/books-library/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/alex/projects/testjob/books-library/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
